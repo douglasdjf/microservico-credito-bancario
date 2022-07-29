1dee1ef1abf8a136374cd671333f904d15e8f5ce
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-07-28 22:59:52 -0300

    Removendo confiugranção antiga do SecurityConfig

--- eurekaserver/src/main/java/com/creditobancario/eurekaserver/SecurityConfig.java
+++ eurekaserver/src/main/java/com/creditobancario/eurekaserver/SecurityConfig.java
@@ -1,15 +1,16 @@
 package com.creditobancario.eurekaserver;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf()
                 .disable()
@@ -17,5 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+        return http.build();
     }
 }
