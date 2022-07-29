f1ecc7981e731e9e8b955777cb2dcae3dafd3f0c
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-07-28 21:45:46 -0300

    Ajustando autenticacao eureka server

--- microservico-cartoes/src/main/java/com/creditobancario/microservicocartoes/MicroservicoCartoesApplication.java
+++ microservico-cartoes/src/main/java/com/creditobancario/microservicocartoes/MicroservicoCartoesApplication.java
@@ -1,13 +1,16 @@
 package com.creditobancario.microservicocartoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@SpringBootApplication
 @EnableEurekaClient
 @EnableRabbit
-@SpringBootApplication
+@Slf4j
 public class MicroservicoCartoesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicoCartoesApplication.class, args);
